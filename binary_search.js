244609002953a5d79334f95db32caa580786a257
Chris <51548719+ZoologyJS@users.noreply.github.com>
2020-05-08 18:54:13 -0700

    Binary Search

---
+++ binary_search.js
@@ -0,0 +1,22 @@
+/*
+** @param {array} items - Array to be searched
+** @param {number} value - Value to search for
+*/
+
+const items = [1, 2, 3, 4, 5, 6];
+
+function binarySearch(items, value){
+  let firstIndex = 0;
+  let lastIndex = items.length - 1;
+  let middleIndex = Math.floor((firstIndex + lastIndex) / 2);
+
+  while (items[middleIndex] != value && firstIndex < lastIndex){
+    if (value < items[middleIndex]){
+      lastIndex = middleIndex - 1;
+    } else if (value > items[middleIndex]){
+      firstIndex = middleIndex + 1;
+    }
+    middleIndex = Math.floor((lastIndex + firstIndex) / 2)
+  }
+  return (items[middleIndex] != value) ? -1 : middleIndex;
+}
